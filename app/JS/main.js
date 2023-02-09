08f315a3e9709a395422b204aba4739e9bdb3edb
TK-LS97 <113810058+TK-LS97@users.noreply.github.com>
2023-02-09 19:35:11 +0200

    Mobile-first finally done

--- app/JS/main.js
+++ app/JS/main.js
@@ -1,6 +1,16 @@
 const ham = document.querySelector(".hamburger");
+const nav = document.querySelector(".nav-mobile");
+const main = document.querySelector(".big-wrap");
+const dim = document.querySelector(".container");
+// document.body.firstElementChild=dim
+// nav.previousElementSibling = dim;
+
 
 
 ham.addEventListener("click",()=>{
     ham.classList.toggle("active");
+    nav.classList.toggle("active");
+    dim.style.display = "block";
 })
+
+console.log(document.body)
