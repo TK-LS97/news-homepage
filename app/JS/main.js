fb9f18a55f29ac07611068e857e975ce5f4af68e
Tokelo Maloi <tokelomaloi@gmail.com>
2023-02-21 15:26:16 +0200

    Update main.js

--- app/JS/main.js
+++ app/JS/main.js
@@ -1,16 +1,15 @@
 const ham = document.querySelector(".hamburger");
 const nav = document.querySelector(".nav-mobile");
-const main = document.querySelector(".big-wrap");
-const dim = document.querySelector(".container");
-// document.body.firstElementChild=dim
-// nav.previousElementSibling = dim;
 
 
 
-ham.addEventListener("click",()=>{
+
+ham.addEventListener("click",()=>{   //When hamburger menu is clicked do this
+    
+    //Toggle and add ".active" class to .hamburger class Element
     ham.classList.toggle("active");
+
+    //same here for .nav-mobile
     nav.classList.toggle("active");
-    dim.style.display = "block";
 })
 
-console.log(document.body)
